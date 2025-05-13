ffe8ff79a810cdd60d37bda34d5ea3cf512c8e7a
Volodymyr Butynets <admin@MacBookPro.Dlink>
2025-05-13 09:23:46 +0300

    Added concatenation + interpolation lesson

---
+++ Lessons/Concatenation-lesson.js
@@ -0,0 +1,7 @@
+//Concatenation
+let price = 79;
+let itemName = "hell"
+let messageToPrint = "The price for your " + itemName + " is " + price + " dollars" //concatenation
+let messageToPrint2 = `The price for your ${itemName} is ${price} dollars` //interpolation
+
+console.log(messageToPrint)
